a33884d046a7bc1e266aa43dfe01cca589ba15b9
Mohammed Zeeshan <93642091+alizeeshan1234@users.noreply.github.com>
2025-08-02 15:12:00 +0530

    Update initialize.rs

--- program/src/instruction/initialize.rs
+++ program/src/instruction/initialize.rs
@@ -1,31 +1,31 @@
-// use pinocchio::{
-//     account_info::AccountInfo,
-//     instruction::{Seed, Signer},
-//     program_error::ProgramError,
-//     pubkey::{self, Pubkey},
-//     sysvars::rent::Rent,
-//     ProgramResult,
-// };
+use pinocchio::{
+    account_info::AccountInfo,
+    instruction::{Seed, Signer},
+    program_error::ProgramError,
+    pubkey::{self, Pubkey},
+    sysvars::rent::Rent,
+    ProgramResult,
+};
 
 
-// use solana_program::stake::state::Authorized;
+use solana_program::stake::state::Authorized;
 
 
-// fn initialize(
-//         accounts: &[AccountInfo],
-//         authorized: Authorized,
-//         lockup: Lockup,
-//     ) -> ProgramResult {
-//         let account_info_iter = &mut accounts.iter();
+fn initialize(
+        accounts: &[AccountInfo],
+        authorized: Authorized,
+        lockup: Lockup,
+    ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
 
-//         // native asserts: 2 accounts (1 sysvar)
-//         let stake_account_info = next_account_info(account_info_iter)?;
-//         let rent_info = next_account_info(account_info_iter)?;
+        // native asserts: 2 accounts (1 sysvar)
+        let stake_account_info = next_account_info(account_info_iter)?;
+        let rent_info = next_account_info(account_info_iter)?;
 
-//         let rent = &Rent::from_account_info(rent_info)?;
+        let rent = &Rent::from_account_info(rent_info)?;
 
-//         // `get_stake_state()` is called unconditionally, which checks owner
-//         do_initialize(stake_account_info, authorized, lockup, rent)?;
+        // `get_stake_state()` is called unconditionally, which checks owner
+        do_initialize(stake_account_info, authorized, lockup, rent)?;
 
-//         Ok(())
-//     }
+        Ok(())
+    }
